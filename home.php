fabbf991ef938d67581903ae0fa68dc9a589a18a
Gerardo Hollmann <hollmanngerardo@yahoo.com.ar>
2021-03-01 18:59:20 -0300

    ''

---
+++ home.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">
+    <title>ESTUDIO JURIDICO HOLLMANN</title>
+  </head>
+  <body style="background-color: #f8f9fa!important;">
+
+    <div class="conteiner">
+        <header class="header">
+                    <nav class="navbar navbar-expand-lg" style="background-color:#343a40!important;">
+            <div class="container-fluid">
+              <h2 style="color:white;text-align:center" class="display-2"> <font size="8" face="Bookman old style"> ESTUDIO JURIDICO HOLLMANN </font></h2>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+                <button class="btn btn-secondary" style="background-color:#828282;" type="button"> <font face="Georgia"><a id="contact" style="color:black;background-color:none;text-decoration:none;" href="mailto:estudiohollmann@yahoo.com.ar">CONTACTO VIA MAIL</a> </font></button>
+              </div>
+            </div>
+          </nav>
+
+        </header>
+        <section class="cuerpo">
+          <br>
+          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="imagenes/estanteria.jpg" class="d-block w-100" alt="...">
+                <br>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5 style="color:black">Estudio Jurídico Hollmann</h5>
+                  <p style="color:black;">El mismo es un estudio con una reconocida trayectoria de mas de 40 años, radicado en la zona de Quilmes. Nos dedicamos especialmente a cuestiones de derecho del trabajo, familia y sucesiones.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="imagenes/sala-de-reuniones-estudio.jpg" class="d-block w-100" alt="...">
+                <br>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Sala de reuniones</h5>
+                  <p>La filosofía del estudio se basa en el respeto del caso, la confidencialidad y la <b><u style="color:black">confianza con el cliente.</u></b></p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="imagenes/despacho.jpg" class="d-block w-100" alt="...">
+                <br>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Dr. Juan Pablo Hollmann</h5>
+                  <p>El Dr. Hollmann es egresado de la Universidad Nacional de La Plata, y ejerce la profesión de manera libre desde el año 1992; siempre se  a dedicado al derecho civil, laboral y de famillia.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" style="background-color:black;" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+              <span class="carousel-control-next-icon" style="background-color:black;" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+          <br>
+          <br>
+          <div class="paginas-interes" style="width:95%;margin-left:auto; margin-right:auto;">
+              <h5 class="display-5">PÁGINAS DE INTERES</h5>
+              <br>
+              <div class="row">
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
+                  <a id="boton1" class="links" href="http://www.scba.gov.ar/portada/default2014.asp"> PODER JUDICIAL PBA</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right">
+                  <a class="links" href="http://www.infoleg.gob.ar/">LEYES NACIONALES</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tooltip on bottom">
+                  <a class="links" href="https://normas.gba.gob.ar/">LEYES PROVINCIALES</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+                  <a class="links" href="https://www.diariojudicial.com/">DIARIO JUDICIAL</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+                  <a class="links" href="https://www.boletinoficial.gba.gob.ar/">BOLETÍN OFICIAL PBA</a>
+                </button>
+              </div>
+          </div>
+          <br>
+          <br>
+          <div class="paginas-interes" style="width:95%;margin-left:auto; margin-right:auto;">
+              <h5 class="display-5">ARTICULOS DE INTERES COMPARTIDOS POR EL DR. HOLLMANN</h5>
+              <br>
+              <div class="row">
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip on top">
+                  <a id="boton1" class="links" href="http://www.scba.gov.ar/portada/default2014.asp"> ej1</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right">
+                  <a class="links" href="http://www.infoleg.gob.ar/">ej2</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Tooltip on bottom">
+                  <a class="links" href="https://normas.gba.gob.ar/">ej3</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+                  <a class="links" href="https://www.diariojudicial.com/">ej4</a>
+                </button>
+                <button type="button" class="col-4 col-md btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" title="Tooltip on left">
+                  <a class="links" href="https://www.boletinoficial.gba.gob.ar/">ej5</a>
+                </button>
+              </div>
+          </div>
+        </section>
+        <footer class="footer">
+          <div class="row" style="margin-left:60%; margin-top:3%;">
+            <div class="col-6 col-md">
+              <h5>Redes Sociales</h5>
+                <ul class="list-unstyled text-small">
+                  <li> <img src="iconos/instagram.svg" style="width:20px;height:20px" alt="logoFB"> <a class="text-muted" href="https://www.instagram.com/">Instagram</a></li>
+                  <li> <img src="iconos/twitter.svg" style="width:20px;height:20px" alt="logoFB"> <a class="text-muted" href="https://twitter.com/?lang=es">Twitter</a></li>
+                  <li> <img src="iconos/linkedin.svg" style="width:20px;height:20px" alt="logoFB"> <a class="text-muted" href="https://www.linkedin.com/in/juan-pablo-hollmann-7a886612/">Linkedin</a></li>
+                  <li> <img src="iconos/facebook.svg" style="width:20px;height:20px" alt="logoFB"> <a class="text-muted" href="https://www.facebook.com/Estudio-Hollmann-371282002883834">Facebook</a></li>
+                </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Datos</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="pop-up-mail">Email</a></li>
+              <li><a class="text-muted" href="pop-up-horario">Horario</a></li>
+              <li><a class="text-muted" href="pop-up-tel.php">(011) 4254-8038</a></li>
+              <li><a class="text-muted" href="https://www.google.com/maps/place/Estudio+Hollmann.+Dr.+Juan+Pablo+Hollmann/@-34.7285152,-58.2638858,15z/data=!4m2!3m1!1s0x0:0x9b638f7214b1daa6?sa=X&ved=2ahUKEwjArs3YpYPvAhV7HrkGHeJUCccQ_BIwCnoECCAQBQ">Pte. Peron 468, Quilmes</a></li>
+            </ul>
+          </div>
+        </div>
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+  </body>
+</html>
